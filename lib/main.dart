e986e8717bbc283090c8b097d6c9d98cc9cc8787
Jacksin76 <122627390+Jacksin76@users.noreply.github.com>
2023-02-13 00:39:00 +0100

    Final Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,173 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Calculator());
+}
 
-
-void main() => runApp(MyCalculator());
-
-class MyCalculator extends StatelessWidget {
-
-
+class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
-      home: Calculator(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: CalcBody(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-
-
-class Calculator extends StatefulWidget {
+class CalcBody extends StatefulWidget {
   @override
-  _CalculatorState createState() => _CalculatorState();
+  _CalcBodyState createState() => _CalcBodyState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  String output = "0";
 
-  String _output = "0";
-  double num1 = 0.0;
-  double num2 = 0.0;
-  String operand = "";
-
-  buttonPressed(String buttonText) {
-    if (buttonText == "CLEAR") {
-      _output = "0";
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else if (buttonText == "+" ||
-        buttonText == "-" ||
-        buttonText == "/" ||
-        buttonText == "X") {
-      num1 = double.parse(output);
-      operand = buttonText;
-      _output = "0";
-    } else if (buttonText == ".") {
-      if (_output.contains(".")) {
-        print("Already contains a decimal");
-        return;
-      } else {
-        _output = _output + buttonText;
-      }
-    } else if (buttonText == "=") {
-      num2 = double.parse(output);
-      if (operand == "+") {
-        _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
-        _output = (num1 - num2).toString();
-      }
-      if (operand == "X") {
-        _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
-        _output = (num1 / num2).toString();
-      }
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else {
-      _output = _output + buttonText;
-    }
-    setState(() {
-      output = double.parse(_output).toStringAsFixed(2);
-    });
-  }
-
-  Widget buildButton(String buttonText) {
-    return TextButton(
-      child: Text(
-        buttonText,
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      onPressed: () {
-        buttonPressed(buttonText);
-      },
-    );
-  }
-
-
-
+class _CalcBodyState extends State<CalcBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Calculator"),
-        ),
-        body: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.symmetric(
-                      vertical: 24.0,
-                      horizontal: 12.0,
-                    ),
-                    child: Text(
-                      output,
-                      style: TextStyle(
-                        fontSize: 48.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )),
-                Expanded(
-                  child: Divider(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
                 ),
-                Column(children: [
-                  Row(children: [
-                    buildButton("7"),
-                    buildButton("8"),
-                    buildButton("9"),
-                    buildButton("/")
-                  ]),
-                  Row(children: [
-                    buildButton("4"),
-                    buildButton("5"),
-                    buildButton("6"),
-                    buildButton("X")
-                  ]),
-                  Row(children: [
-                    buildButton("1"),
-                    buildButton("2"),
-                    buildButton("3"),
-                    buildButton("-")
-                  ]),
-                  Row(children: [
-                    buildButton("."),
-                    buildButton("0"),
-                    buildButton("00"),
-                    buildButton("+")
-                  ]),
-                  Row(children: [
-                    buildButton("CLEAR"),
-                    buildButton("="),
-                  ]),
-                ])
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                FunkctionButton("C"),
+                FunkctionButton("⌫"),
+                FunkctionButton("%"),
+                FunkctionButton("/"),
               ],
-            )));
+            ),
+            Row(
+              children: <Widget>[
+                NumberButton("7"),
+                NumberButton("8"),
+                NumberButton("9"),
+                FunkctionButton("+"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                NumberButton("4"),
+                NumberButton("5"),
+                NumberButton("6"),
+                FunkctionButton("-"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                NumberButton("1"),
+                NumberButton("2"),
+                NumberButton("3"),
+                FunkctionButton("x"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                FunkctionButton("."),
+                NumberButton("0"),
+                NumberButton("00"),
+                FunkctionButton("="),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget FunkctionButton(String val) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => btnClicked(val),
+        child: Text(
+          val,
+          style: const TextStyle(fontSize: 35.0, color: Colors.deepOrange),
+        ),
+      ),
+    );
+  }
+
+  Widget NumberButton(String val) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => btnClicked(val),
+        child: Text(
+          val,
+          style: const TextStyle(fontSize: 35.0, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  String text = "0";
+  String result = "0";
+  String expression = "";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operation = "";
+
+  void btnClicked(String val) {
+    if (val == "C") {
+      text = "0";
+      result = "0";
+      expression = "";
+      num1 = 0.0;
+      num2 = 0.0;
+      operation = "";
+    } else if (val == "⌫") {
+      text = text.substring(0, text.length - 1);
+      if (text == "") {
+        text = "0";
+      }
+    } else if (val == "+" ||
+        val == "-" ||
+        val == "x" ||
+        val == "/" ||
+        val == "%") {
+      num1 = double.parse(text);
+      operation = val;
+      expression = text + operation;
+      text = "0";
+    } else if (val == "=") {
+      num2 = double.parse(text);
+      if (operation == "+") {
+        result = (num1 + num2).toString();
+      }
+      if (operation == "-") {
+        result = (num1 - num2).toString();
+      }
+      if (operation == "x") {
+        result = (num1 * num2).toString();
+      }
+      if (operation == "/") {
+        result = (num1 / num2).toString();
+      }
+      if (operation == "%") {
+        result = (num1 / 100 * num2).toString();
+      }
+      expression = "";
+      text = result;
+    } else {
+      if (text == "0") {
+        text = val;
+      } else {
+        text = text + val;
+      }
+    }
+    setState(() {});
   }
 }
-
